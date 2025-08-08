4747eb5559ccf3735709eef768105b31976b6bbf
Shelby Norris <shelbs9109@gmail.com>
2025-08-07 20:44:04 -0500

    Created all files with Vite npm build command, edited and/or removed boiler plate code from App.css, App.jsx, index.css, and README.md files to start project with a blank slate and to add 'coming soon' update to README.md for Github

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>myWeather App</h1>
+        <p>Your personal weatherman!</p>
+      </header>
+      <main>
+        
+      </main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
