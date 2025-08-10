a0c70f6f0a81d5eef78ff5d91e13f2f431b70804
Shelby Norris <shelbs9109@gmail.com>
2025-08-10 16:57:54 -0500

    centered text, changed font in App.css. In Weather.jsx, added loading message while weather data is being fetched, added error message if city name is incorrect or nonexistent, created function to display desired weather data in a div container, edited the API URL to change temp units from Kelvin to Farenheit/Imperial

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -3,12 +3,30 @@ import { useState, useEffect } from "react";
 export default function Weather() {
   const [city, setCity] = useState("New Orleans");
   const [weatherData, setWeatherData] = useState(null);
+  // handling loading
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  function displayWeather() {
+    if (weatherData) {
+      return (
+        <div id="weather-div">
+          <p>Description: <span>{weatherData.weather[0].description}</span></p>
+          <p>Temperature: <span>{weatherData.main.temp}</span> °F</p>
+          <p>Feels Like: <span>{weatherData.main.feels_like}</span> °F</p>
+
+        </div>
+    )
+  }
+  };
+
 
   useEffect(() => {
     // code to run side effects, like fetching data
     async function getWeather() {
+      setLoading(true); // start loading
       const key = import.meta.env.VITE_OPEN_WEATHER_MAP_API_KEY; // keeps your API key secure
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=imperial`;
       try {
         const response = await fetch(url);
         if (!response.ok) {
@@ -19,8 +37,9 @@ export default function Weather() {
         console.log(result);
       } catch (error) {
         console.error(error);
+        setError(true);
       } finally {
-        //
+        setLoading(false); // stop loading
       }
     }
 
@@ -31,10 +50,12 @@ export default function Weather() {
     <>
       <section>
         <h2>Current Weather for {city}</h2>
-        {weatherData && weatherData.weather[0].main} <br />
-        {weatherData && weatherData.weather[0].description}
 
+        {loading && <p>Loading...</p>}
 
+        {error && <p>Oops! Something went wrong!</p>}
+
+        {displayWeather()}
 
         {/* <form>
         <label htmlFor="city">Enter a City</label>
